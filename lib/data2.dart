bcf3bd1f5256e8e98347a6381472fc0d8421872f
Adriano Junior <mand458@hotmail.com>
2023-12-01 17:29:41 -0300

    ajuste nas perguntas e alternativas

--- lib/data2.dart
+++ lib/data2.dart
@@ -1,102 +1,111 @@
 final List quiz = [
   {
-    "pergunta": "O que é um ataque de força bruta?",
+    "pergunta":
+        "O que é um ataque de força bruta?\n\n 1- Um ataque que usa a força física para danificar equipamentos de TI\n\n 2- Um ataque que tenta adivinhar uma senha testando todas as combinações possíveis\n\n 3- Um ataque que explora vulnerabilidades em um sistema operacional\n\n 4- Um ataque que engana um usuário para revelar suas credenciais\n",
     "respostas": [
-      "Um ataque que usa a força física para danificar equipamentos de TI",
-      "Um ataque que tenta adivinhar uma senha testando todas as combinações possíveis",
-      "Um ataque que explora vulnerabilidades em um sistema operacional",
-      "Um ataque que engana um usuário para revelar suas credenciais",
+      "1",
+      "2",
+      "3",
+      "4",
     ],
     "alternativa_correta": 2,
   },
   {
-    "pergunta": "O que é um ataque de negação de serviço (DoS)?",
+    "pergunta":
+        "O que é um ataque de negação de serviço (DoS)?\n\n 1- Um ataque que sobrecarrega um sistema com tráfego para torná-lo inacessível\n\n 2- Um ataque que rouba informações confidenciais\n\n 3 - Um ataque que danifica fisicamente a infraestrutura de TI\n\n 4 - Um ataque que envia spam para um usuário\n",
     "respostas": [
-      "Um ataque que sobrecarrega um sistema com tráfego para torná-lo inacessível",
-      "Um ataque que rouba informações confidenciais",
-      "Um ataque que danifica fisicamente a infraestrutura de TI",
-      "Um ataque que envia spam para um usuário",
-    ],
-    "alternativa_correta": 1,
-  },
-  {
-    "pergunta": "O que é phishing?",
-    "respostas": [
-      "Um tipo de malware",
-      "Um tipo de ataque cibernético que envolve o envio de e-mails fraudulentos para enganar os destinatários a revelar informações pessoais",
-      "Um tipo de criptografia",
-      "Um protocolo de rede seguro",
-    ],
-    "alternativa_correta": 2,
-  },
-  {
-    "pergunta": "O que é um firewall?",
-    "respostas": [
-      "Um tipo de vírus de computador",
-      "Um dispositivo de segurança de rede que monitora e controla o tráfego de rede com base em regras de segurança predeterminadas",
-      "Um programa de computador que protege contra spam",
-      "Um tipo de ataque cibernético",
-    ],
-    "alternativa_correta": 2,
-  },
-  {
-    "pergunta": "O que é criptografia?",
-    "respostas": [
-      "Um tipo de malware",
-      "A prática de proteger informações transformando-as em um código para impedir o acesso não autorizado",
-      "Um método para desenvolver software seguro",
-      "Um protocolo de rede seguro",
-    ],
-    "alternativa_correta": 2,
-  },
-  {
-    "pergunta": "O que é autenticação de dois fatores (2FA)?",
-    "respostas": [
-      "Um tipo de ataque cibernético",
-      "Um tipo de criptografia",
-      "Um método de autenticação que requer dois tipos diferentes de informações para verificar a identidade de um usuário",
-      "Um protocolo de rede seguro",
-    ],
-    "alternativa_correta": 3,
-  },
-  {
-    "pergunta": "O que é um ataque de homem no meio (MitM)?",
-    "respostas": [
-      "Um tipo de ataque cibernético onde um invasor intercepta a comunicação entre duas partes",
-      "Um tipo de malware",
-      "Um tipo de criptografia",
-      "Um protocolo de rede seguro",
+      "1",
+      "2",
+      "3",
+      "4",
     ],
     "alternativa_correta": 1,
   },
   {
     "pergunta":
-        "O que é um cavalo de Tróia em termos de segurança cibernética?",
+        "O que é phishing? 1- Um tipo de malware\n\n 2 - Um tipo de ataque cibernético que envolve o envio de e-mails fraudulentos para enganar os destinatários a revelar informações pessoais\n\n 3 - Um tipo de criptografia\n\n 4 - Um protocolo de rede seguro\n",
     "respostas": [
-      "Um tipo de ataque cibernético que envolve o envio de e-mails fraudulentos",
-      "Um tipo de malware que se disfarça como um software legítimo",
-      "Um tipo de criptografia",
-      "Um protocolo de rede seguro",
+      "1",
+      "2",
+      "3",
+      "4",
     ],
     "alternativa_correta": 2,
   },
   {
-    "pergunta": "O que é engenharia social em segurança cibernética?",
+    "pergunta":
+        "O que é um firewall?\n\n 1- Um tipo de vírus de computador\n\n 2- Um dispositivo de segurança de rede que monitora e controla o tráfego de rede com base em regras de segurança predeterminadas\n\n 3- Um programa de computador que protege contra spam\n\n 4- Um tipo de ataque cibernético\n",
     "respostas": [
-      "Um tipo de ataque cibernético que envolve manipular pessoas para que revelem informações confidenciais",
-      "Um tipo de malware",
-      "Um tipo de criptografia",
-      "Um protocolo de rede seguro",
+      "1",
+      "2",
+      "3",
+      "4",
+    ],
+    "alternativa_correta": 2,
+  },
+  {
+    "pergunta":
+        "O que é criptografia?\n\n 1 - Um tipo de malware\n\n 2- A prática de proteger informações transformando-as em um código para impedir o acesso não autorizado\n\n 3 - Um método para desenvolver software seguro\n\n 4 - Um protocolo de rede seguro\n",
+    "respostas": [
+      "1",
+      "2",
+      "3",
+      "4",
+    ],
+    "alternativa_correta": 2,
+  },
+  {
+    "pergunta":
+        "O que é autenticação de dois fatores (2FA)?\n\n 1 - Um tipo de ataque cibernético\n\n 2- Um tipo de criptografia\n\n 3 - Um método de autenticação que requer dois tipos diferentes de informações para verificar a identidade de um usuário\n\n 4 - Um protocolo de rede seguro\n",
+    "respostas": [
+      "1",
+      "2",
+      "3",
+      "4",
+    ],
+    "alternativa_correta": 3,
+  },
+  {
+    "pergunta":
+        "O que é um ataque de homem no meio (MitM)?\n\n 1 - Um tipo de ataque cibernético onde um invasor intercepta a comunicação entre duas partes\n\n 2 - Um tipo de malware\n\n 3 - Um tipo de criptografia\n\n 4- Um protocolo de rede seguro\n",
+    "respostas": [
+      "1",
+      "2",
+      "3",
+      "4",
     ],
     "alternativa_correta": 1,
   },
   {
-    "pergunta": "O que é um ataque de injeção SQL?",
+    "pergunta":
+        "O que é um cavalo de Tróia em termos de segurança cibernética?\n\n 1 - Um tipo de ataque cibernético que envolve o envio de e-mails fraudulentos\n\n 2 - Um tipo de malware que se disfarça como um software legítimo\n\n 3 - Um tipo de criptografia\n\n 4 - Um protocolo de rede seguro\n",
     "respostas": [
-      "Um tipo de ataque cibernético que envolve o envio de e-mails fraudulentos",
-      "Um tipo de malware",
-      "Um tipo de ataque cibernético que envolve a inserção de código SQL malicioso em uma consulta",
-      "Um protocolo de rede seguro",
+      "1",
+      "2",
+      "3",
+      "4",
+    ],
+    "alternativa_correta": 2,
+  },
+  {
+    "pergunta":
+        "O que é engenharia social em segurança cibernética?\n\n 1- Um tipo de ataque cibernético que envolve manipular pessoas para que revelem informações confidenciais\n\n 2 - Um tipo de malware\n\n 3 - Um tipo de criptografia\n\n 4 - Um protocolo de rede seguro\n",
+    "respostas": [
+      "1",
+      "2",
+      "3",
+      "4",
+    ],
+    "alternativa_correta": 1,
+  },
+  {
+    "pergunta":
+        "O que é um ataque de injeção SQL?\n\n 1 - Um tipo de ataque cibernético que envolve o envio de e-mails fraudulentos\n\n 2 - Um tipo de malware\n\n 3 - Um tipo de ataque cibernético que envolve a inserção de código SQL malicioso em uma consulta\n\n 4 - Um protocolo de rede seguro\n",
+    "respostas": [
+      "1",
+      "2",
+      "3",
+      "4",
     ],
     "alternativa_correta": 3,
   },
